2021bcff04907265060b1d5051ace159648ec4f0
triagung128 <triagung128@gmail.com>
2022-12-17 04:22:43 +0700

    create home page

---
+++ lib/widgets/product_tile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class ProductTile extends StatelessWidget {
+  const ProductTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        left: defaultMargin,
+        right: defaultMargin,
+        bottom: defaultMargin,
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset(
+              'assets/image_shoes.png',
+              width: 120,
+              height: 120,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 12),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Football',
+                  style: secondaryTextStyle.copyWith(fontSize: 12),
+                ),
+                const SizedBox(height: 6),
+                Text(
+                  'Predator 20.3 Firm Ground',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: semiBold,
+                  ),
+                  maxLines: 2,
+                ),
+                const SizedBox(height: 6),
+                Text(
+                  '\$68,47',
+                  style: priceTextStyle.copyWith(fontWeight: medium),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
